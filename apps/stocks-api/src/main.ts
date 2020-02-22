eaf30c0ce0c0bd2dcb237372fc52ffd2930feaee
Sharma <mohit.y.sharma@accenture.com>
2020-02-22 12:11:51 +0530

    routed call

--- apps/stocks-api/src/main.ts
+++ apps/stocks-api/src/main.ts
@@ -3,6 +3,65 @@
  * This is only a minimal backend to get started.
  **/
 import { Server } from 'hapi';
+import axios from 'axios';
+import CacheService from './cache.service';
+
+const ttl = 60 * 60 * 1; // cache for 1 Hour
+const cache = new CacheService(ttl); // Create a new cache service instance
+
+
+const stocksHandler = async (request, h) => {
+  let data;
+
+  // const key = `stock_${request.params.symbol}_${request.params.period}`;
+
+  // const cachedValue = cache.get(key)
+
+  // if(cachedValue) {
+  //    return cachedValue;
+  // }
+
+  try {
+
+    await axios
+      .get(
+        `https://sandbox.iexapis.com/beta/stock/${
+          request.params.symbol
+        }/chart/${request.params.period}?token=${request.query.token}`
+      )
+      .then(resp => {
+        data = resp && resp.data;
+      })
+      .catch(err => console.log('error=>', err));
+
+    return h.response(data).code(200);
+  } catch (err) {
+    console.log('err==>', err);
+    throw new Error(err);
+  }
+};
+
+
+
+
+const defaultHandler = (request, h) => {
+  return {
+    hello: 'world'
+  };
+};
+
+const routes = [
+  {
+    path: '/stock/{symbol}/chart/{period}',
+    method: 'GET',
+    handler: stocksHandler
+  },
+  {
+    method: 'GET',
+    path: '/',
+    handler: defaultHandler
+  }
+];
 
 const init = async () => {
   const server = new Server({
@@ -10,15 +69,7 @@ const init = async () => {
     host: 'localhost'
   });
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-      return {
-        hello: 'world'
-      };
-    }
-  });
+  server.route(routes);
 
   await server.start();
   console.log('Server running on %s', server.info.uri);
